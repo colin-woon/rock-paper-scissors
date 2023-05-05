01b6bf0e4350e36ad0f94b069b0f5e1f42727d56
Colin Woon <128688438+colin-woon@users.noreply.github.com>
2023-05-05 17:33:42 +0800

    Complete function for random computer choice
    - Create array of choices
    - Generate a random index for the array
    - Use random generated index to get element from index

--- game.js
+++ game.js
@@ -1,7 +1,26 @@
-// 
-function getComputerChoice() { }
+// Prompt player to choose RPS (0=Rock, 1=Paper, 2=Scissors)
+// Compare player selection and computer selection
+
+
+// Generate random computer choice for RPS
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"]
+    //Gets random index from choices (0-2)
+    let rand = Math.floor(Math.random() * choices.length);
+    //Gets element based on the random index generated
+    let computerSelection = choices[rand];
+    return computerSelection;
+}
 function gameplay(playerSelection, computerSelection) {
     let realPlayerSelection = playerSelection.toLowerCase();
     let realComputerSelection = computerSelection.toLowerCase();
 }
-function game() { }
+function game() {
+    console.log(`Welcome to Rock Paper Scissors! Game of 5! 
+    Choose your selection:
+    1 = Rock,
+    2 = Scissors,
+    3 = Paper`)
+
+
+}
