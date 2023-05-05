d32ecfc49f9a79278c50e0a1ce5b8dfbea1895e7
Colin Woon <128688438+colin-woon@users.noreply.github.com>
2023-05-05 12:34:33 +0800

    Add initial required functions to js file

---
+++ game.js
@@ -0,0 +1,7 @@
+// 
+function getComputerChoice() { }
+function gameplay(playerSelection, computerSelection) {
+    let realPlayerSelection = playerSelection.toLowerCase();
+    let realComputerSelection = computerSelection.toLowerCase();
+}
+function game() { }
